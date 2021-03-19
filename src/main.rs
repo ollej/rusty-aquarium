d90aa70ea12324fc0aa0256513292a0c976307f6
Olle Wreede <olle@wreede.se>
2021-03-19 23:48:01 +0100

    Fix randomized start position

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,25 @@ impl Fish {
     const MAX_POSITION: Vec2 = Vec2 { x: 5., y: 10. };
     const MIN_POSITION: Vec2 = Vec2 { x: 5., y: 10. };
     const DIRECTION_CHANGE_CHANCE: Vec2 = Vec2 { x: 2., y: 0.5 };
+    const SIZE: f32 = 10.;
 
-    fn new(start_position: Vec2, screen_size: Vec2, texture: Texture2D) -> Fish {
+    fn new(screen_size: Vec2, texture: Texture2D) -> Fish {
+        let fish_height = Fish::SIZE / (texture.width() / texture.height());
+        let start_position = vec2(
+            rand::gen_range(Fish::MIN_POSITION.x, screen_size.x - Fish::MAX_POSITION.x - Fish::SIZE - 1.),
+            rand::gen_range(Fish::MIN_POSITION.y, screen_size.y - Fish::MAX_POSITION.y - fish_height - 1.));
         Fish {
             screen_size,
             position: start_position,
             speed: Vec2 { x: 25., y: 7. },
-            size: Vec2 { x: 10., y: 10. / (texture.width() / texture.height()) },
+            size: Vec2 { x: Fish::SIZE, y: fish_height },
             texture: texture,
         }
     }
 
     fn tick(&mut self, delta: f32) {
+        self.update_position(delta);
+
         // Change X direction
         if self.position.x < Fish::MIN_POSITION.x
             || self.position.x > (self.screen_size.x - Fish::MAX_POSITION.x - self.size.x)
@@ -36,8 +43,6 @@ impl Fish {
                 || rand::gen_range(0., 100.) < Fish::DIRECTION_CHANGE_CHANCE.y {
             self.speed.y *= -1.;
         }
-
-        self.update_position(delta);
     }
 
     fn update_position(&mut self, delta: f32) {
@@ -80,10 +85,7 @@ async fn main() {
     let mut fishies = Vec::new();
 
     for _ in 0..10 {
-        let start_pos = vec2(
-            rand::gen_range(Fish::MIN_POSITION.x, Fish::MAX_POSITION.x),
-            rand::gen_range(Fish::MIN_POSITION.y, Fish::MAX_POSITION.y));
-        fishies.push(Fish::new(start_pos, screen_size, fish_texture));
+        fishies.push(Fish::new(screen_size, fish_texture));
     }
 
     // build camera with following coordinate system:
