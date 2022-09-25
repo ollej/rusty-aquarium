e36d3c0afa210e1d05ebc0d7f89160e8f287c743
Olle Wreede <olle@wreede.se>
2022-09-25 22:12:29 +0200

    Update systemdata to work with changes in sysinfo crate

--- src/bin/systemdata.rs
+++ src/bin/systemdata.rs
@@ -1,6 +1,6 @@
 use nanoserde::SerJson;
 use rusty_aquarium::{fish_data::FishData, input_data::InputData};
-use sysinfo::{DiskExt, Process, ProcessExt, ProcessorExt, System, SystemExt};
+use sysinfo::{CpuExt, DiskExt, Process, ProcessExt, System, SystemExt};
 
 fn clamp(value: f32) -> f32 {
     if value < 0.1 {
@@ -17,9 +17,9 @@ fn main() {
     sys.refresh_all();
     let mut fishes = Vec::new();
 
-    for disk in sys.get_disks() {
-        //println!("{} {} {}", disk.get_available_space() as f32, disk.get_total_space(), disk.get_available_space() as f32 / disk.get_total_space() as f32);
-        let size = clamp(1. - disk.get_available_space() as f32 / disk.get_total_space() as f32);
+    for disk in sys.disks() {
+        //println!("{} {} {}", disk.available_space() as f32, disk.total_space(), disk.available_space() as f32 / disk.total_space() as f32);
+        let size = clamp(1. - disk.available_space() as f32 / disk.total_space() as f32);
         fishes.push(FishData {
             fish: "clownfish".to_string(),
             size: size,
@@ -28,7 +28,7 @@ fn main() {
         });
     }
 
-    let memory_size = clamp(sys.get_used_memory() as f32 / sys.get_total_memory() as f32);
+    let memory_size = clamp(sys.used_memory() as f32 / sys.total_memory() as f32);
     fishes.push(FishData {
         fish: "turtle".to_string(),
         size: memory_size,
@@ -36,10 +36,10 @@ fn main() {
         bubbles: 1.0,
     });
 
-    // Number of processors
-    for processor in sys.get_processors() {
-        //println!("{}", processor.get_cpu_usage());
-        let size = clamp(processor.get_cpu_usage() / 100.);
+    // Number of CPUs
+    for cpu in sys.cpus() {
+        //println!("{}", processor.cpu_usage());
+        let size = clamp(cpu.cpu_usage() / 100.);
         fishes.push(FishData {
             fish: "goldfish".to_string(),
             size: size,
@@ -50,26 +50,26 @@ fn main() {
 
     fishes.push(FishData {
         fish: "royalgramma".to_string(),
-        size: sys.get_load_average().one as f32,
+        size: sys.load_average().one as f32,
         speed: 1.0,
         bubbles: 1.0,
     });
     fishes.push(FishData {
         fish: "royalgramma".to_string(),
-        size: sys.get_load_average().five as f32,
+        size: sys.load_average().five as f32,
         speed: 1.0,
         bubbles: 1.0,
     });
     fishes.push(FishData {
         fish: "royalgramma".to_string(),
-        size: sys.get_load_average().fifteen as f32,
+        size: sys.load_average().fifteen as f32,
         speed: 1.0,
         bubbles: 1.0,
     });
 
-    let total_memory = sys.get_total_memory() as f32;
+    let total_memory = sys.total_memory() as f32;
     let processes: Vec<&Process> = sys
-        .get_processes()
+        .processes()
         .into_iter()
         .map(|(_pid, process)| process)
         .filter(|process| process.memory() > 0)
@@ -78,7 +78,7 @@ fn main() {
         //println!("{}: {}, {}", process.name(), process.memory(), process.cpu_usage());
         let size = clamp(process.memory() as f32 / total_memory * 10.);
         let speed = clamp(process.cpu_usage() as f32 * 10.);
-        //println!("{} / {} / {}", size, process.memory() / 1024, sys.get_total_memory() / 1024);
+        //println!("{} / {} / {}", size, process.memory() / 1024, sys.total_memory() / 1024);
         fishes.push(FishData {
             fish: "neontetra".to_string(),
             size,
@@ -86,7 +86,7 @@ fn main() {
             bubbles: 1.0,
         });
     }
-    //println!("{}", sys.get_total_memory());
+    //println!("{}", sys.total_memory());
 
     let data = InputData {
         school: fishes,
