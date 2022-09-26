cbe5596ca1dce78c4e9a48cd34a6d023e591d475
Olle Wreede <olle@wreede.se>
2022-09-26 12:51:55 +0200

    Reuse structs from library

--- src/bin/googlesheetsdata.rs
+++ src/bin/googlesheetsdata.rs
@@ -1,6 +1,7 @@
 extern crate google_sheets4 as sheets4;
 use core::num::ParseIntError;
-use serde::Serialize;
+use nanoserde::SerJson;
+use rusty_aquarium::{fish_data::FishData, input_data::InputData};
 use sheets4::{api::ValueRange, hyper, hyper_rustls, oauth2, Sheets};
 use std::{
     fs,
@@ -10,32 +11,17 @@ use std::{
 use structopt::StructOpt;
 use tokio::{task, time};
 
-#[derive(Debug, Serialize, Clone)]
-pub struct FishData {
-    fish: String,
-    size: f32,
-    speed: f32,
-    bubbles: f32,
+fn fish_data_from_vec(cells: &Vec<String>) -> Option<FishData> {
+    cells.get(2).map(|fish| FishData {
+        fish: fish.to_string(),
+        size: cells.get(3).map_or(1.0, parse_fish_data),
+        speed: cells.get(4).map_or(1.0, parse_fish_data),
+        bubbles: cells.get(5).map_or(1.0, parse_fish_data),
+    })
 }
 
-impl FishData {
-    fn from_vec(cells: &Vec<String>) -> Option<Self> {
-        cells.get(2).map(|fish| FishData {
-            fish: fish.to_string(),
-            size: cells.get(3).map_or(1.0, Self::parse),
-            speed: cells.get(4).map_or(1.0, Self::parse),
-            bubbles: cells.get(5).map_or(1.0, Self::parse),
-        })
-    }
-
-    fn parse(value: &String) -> f32 {
-        value.parse::<f32>().unwrap_or(1.0)
-    }
-}
-
-#[derive(Debug, Serialize)]
-pub struct InputData {
-    school: Vec<FishData>,
+fn parse_fish_data(value: &String) -> f32 {
+    value.parse::<f32>().unwrap_or(1.0)
 }
 
 async fn connect_to_sheets_api(credentials: &PathBuf, tokencache: &PathBuf) -> Sheets {
@@ -90,7 +76,7 @@ async fn fetch_and_convert(hub: &Sheets, spreadsheet: &String, range: &String, o
 
 fn convert_data(data: ValueRange, output_path: &Path) {
     if let Some(input_data) = parse_data(data) {
-        let json = serde_json::to_string(&input_data).expect("Failed generating JSON");
+        let json = SerJson::serialize_json(&input_data);
         write_file(output_path, json);
     }
 }
@@ -100,13 +86,16 @@ fn parse_data(data: ValueRange) -> Option<InputData> {
         let mut fishes = Vec::new();
         for row in values.iter().skip(1) {
             let count: usize = row[1].parse().unwrap_or(1);
-            if let Some(fish) = FishData::from_vec(row) {
+            if let Some(fish) = fish_data_from_vec(row) {
                 for _ in 0..count {
                     fishes.push(fish.clone());
                 }
             }
         }
-        InputData { school: fishes }
+        InputData {
+            school: fishes,
+            legend: None,
+        }
     })
 }
 
